fe831f32070c15e1c3ee1bf91160fe182e44a9b3
Dfelix <davi1103felix@gmail.com>
2025-05-06 09:24:32 -0300

    Add files via upload

---
+++ calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Aluno
+ */
+
+
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("olá");
+        System.out.println("Bom dia" + "para todos ");
+
+        // TODO code application logic here
+    }
+    
+}
